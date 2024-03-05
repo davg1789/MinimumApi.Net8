8d662a73480249dd50df4e0d4fd3ff7766a11a21
Daniel <davg1789@gmail.com>
2024-03-05 14:50:34 +0000

    Add project files.

---
+++ MinimalApi/Context/LocationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApi.Domain;
+
+namespace MinimalApi.Context
+{
+    public class LocationContext : DbContext
+    {
+        public LocationContext(DbContextOptions<LocationContext> options) : base(options)
+        {
+        }
+        public DbSet<Location> Location { get; set; }
+    }
+}
