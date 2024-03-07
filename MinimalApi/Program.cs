755509bef1b4fea5380d148be08849ed06d5cac6
Daniel <davg1789@gmail.com>
2024-03-07 15:12:45 +0000

    Added memory cache

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOutputCache(opt =>
+{
+    opt.DefaultExpirationTimeSpan = TimeSpan.FromSeconds(30);     
+});
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<LocationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -19,10 +23,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseOutputCache();
 
 app.MapGet("/location", async (LocationContext context) =>
 await context.Location.ToListAsync())
 .WithName("location")
-.WithOpenApi();
+.WithOpenApi()
+.CacheOutput();
+
 
 app.Run();
