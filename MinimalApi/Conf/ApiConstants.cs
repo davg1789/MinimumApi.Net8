aee96e3b309ca9ebdc4d4eaf172e5d6a6e5bcc5c
Daniel <davg1789@gmail.com>
2024-04-02 16:53:02 +0100

    ApiKey and encrypted connection string

---
+++ MinimalApi/Conf/ApiConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace MinimalApi.Conf
+{
+    public class ApiConstants
+    {
+        public const string ApiHeaderKey = "api-key";
+        public const string ApiSecret = "ApiSecret";
+    }
+}
---
+++ MinimalApi/Middlewares/ApiKeyAuthenticationMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using MinimalApi.Conf;
+using System.Net;
+
+namespace MinimalApi.Middlewares
+{
+    public class ApiKeyAuthenticationMiddleware(RequestDelegate next, IConfiguration config)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(!context.Request.Headers.TryGetValue(ApiConstants.ApiHeaderKey, out var actualApiKey))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                await context.Response.WriteAsync("Api key is missing.");
+                return;
+            }
+
+            if (!actualApiKey.Equals(config.GetValue<string>(ApiConstants.ApiSecret)))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                await context.Response.WriteAsync("Api key is invalid.");
+                return;
+            }
+
+            await next(context);
+        }
+    }
+}
